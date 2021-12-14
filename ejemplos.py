557119e2f6f0e71ecb22913f6574236dab6325ca
secoz <geduardo2@yahoo.com>
2021-12-14 07:45:17 -0600

    Create ejemplos.py
    En este archivo se grabara partes cortas de codigo con trucos de python que quiero recordar

---
+++ ejemplos.py
@@ -0,0 +1,10 @@
+#ejemplo de manejo de listas
+lista=[10,20,30,40,50,60]
+
+#codigo en una sola line de for y enumarate
+for i,x in enumerate(lista):print(i,x)
+
+
+#ejemplo de un if en una sola linea
+edad=12
+print('es mayor de edad') if edad>=18 else print ('es menor de edad')
